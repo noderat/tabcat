63d8818870862458490a8991c9fdc5d0b634975b
Nathan Norton <nate@pixelandline.com>
2015-11-13 01:19:25 +0000

    Updated dependencies for build system

--- build/gulpfile.coffee
+++ build/gulpfile.coffee
@@ -3,6 +3,7 @@ coffee = require 'gulp-coffee'
 coffeelint = require 'gulp-coffeelint'
 stylish = require 'coffeelint-stylish'
 gutil = require 'gulp-util'
+sass = require 'gulp-sass'
 
 
 parameters = require '../config/parameters.coffee'
@@ -24,4 +25,36 @@ gulp.task 'coffee', ->
         .pipe gulp.dest path
         .on 'error', gutil.log
 
-gulp.task 'default', ['cofeelint', 'coffee']
+gulp.task 'express', ->
+  express = require('express')
+  app = express()
+  app.use require('connect-livereload')(port: 35729)
+  app.use express.static(__dirname)
+  app.listen 4000, '0.0.0.0'
+  return
+
+tinylr = undefined
+gulp.task 'livereload', ->
+  tinylr = require('tiny-lr')()
+  tinylr.listen 35729
+  return
+
+notifyLiveReload = (event) ->
+  fileName = require('path').relative(__dirname, event.path)
+  tinylr.changed body: files: [ fileName ]
+  return
+
+gulp.task 'styles', ->
+  sass('sass', style: 'expanded')
+    .pipe(gulp.dest('css'))
+    .pipe(rename(suffix: '.min'))
+    .pipe(minifycss())
+    .pipe gulp.dest('css')
+
+gulp.task 'watch', ->
+  gulp.watch 'sass/*.scss', [ 'styles' ]
+  gulp.watch '*.html', notifyLiveReload
+  gulp.watch 'css/*.css', notifyLiveReload
+  return
+
+gulp.task 'default', ['cofeelint', 'coffee', 'express', 'livereload']
--- init.sh
+++ init.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 sudo apt-get update -y    ##this step will add 5-10 minutes in initial provision
-sudo apt-get install curl tar vim git -y #nodejs npm couchdb kmod-VirtualBox -y
-#sudo npm install -g kanso coffee-script uglify-js coffeelint -y
-
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
-sudo apt-get install couchdb -y
+sudo apt-get install curl tar vim git couchdb -y
+#sudo apt-get install couchdb -y
 sudo apt-get autoremove
 sudo chown -R couchdb /var/run/couchdb
-sudo chown -R vagrant /home/vagrant/.npm
-#do NOT install nodejs as root user
-apt-get install nodejs -y
-npm install -g npm@3.x-latest
+
+#install nvm to avoid permissions issues
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+#because source ~/.bashrc did not work
+source ~/.nvm/nvm.sh
+nvm install stable -y
+nvm use stable
+#so that we can use node right away
+nvm alias default node
+
 #installing these globally for now until they can be used with gulp
-npm install -g kanso coffee-script uglify-js coffeelint -y
+npm install -g gulp kanso coffee-script uglify-js coffeelint -y
+
+#required for sass compilation
 sudo su -c "gem install sass"
-#skipping npm install for now
+
+#skipping npm install for now, re-enable later
 #cd /vagrant && npm install
